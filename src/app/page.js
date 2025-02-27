aa87824fd65b88c18d3eba67c83610353a2aeba1
Ponlork <ponlork@Ponlorks-MacBook-Air.local>
2025-02-27 13:25:58 +0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,151 @@
-import Image from "next/image";
+//import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-white-500 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="w-full p-4">
+        <nav>
+          <ul className="flex gap-6 justify-center">
+            <li>
+              <a
+                href="#about"
+                className="no-underline text-orange-300 hover:text-blue-500"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#experience"
+                className="no-underline text-orange-300 hover:text-blue-500"
+              >
+                Experience
+              </a>
+            </li>
+            <li>
+              <a
+                href="#education"
+                className="no-underline text-orange-300 hover:text-blue-500"
+              >
+                Education
+              </a>
+            </li>
+            <li>
+              <a
+                href="#skills"
+                className="no-underline text-orange-300 hover:text-blue-500"
+              >
+                Skills
+              </a>
+            </li>
+            <li>
+              <a
+                href="#project"
+                className="no-underline text-orange-300 hover:text-blue-500"
+              >
+                Project
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className="no-underline text-orange-300 hover:text-blue-500"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      <main className="w-full">
+        <section id="about" className="text-center py-10">
+          <h1 className="text-3xl font-bold font-mono text-orange-500 flex justify-left">
+            Hi, I am {"  "}
+            <span className="text-3xl font-mono text-blue-500 flex justify-left">
+              PONLORK
+            </span>
+          </h1>
+          <img></img>
+          <p className="text-lg">
+            Email: ponlork2535@gmail.com | Phone: +855 97 510 8710
+          </p>
+          <p>Address: Kein Svay, Kandal Province, Cambodia.</p>
+          <p>
+            I am a third-year student at Institute of Technology of Cambodia
+            (ITC) majoring in Computer Science.
+          </p>
+        </section>
+
+        <section id="experience" className="text-center py-10">
+          <h2 className="text-red-500">Work Experience</h2>
+          <p>No Experience</p>
+        </section>
+
+        <section id="education" className="text-center py-10">
+          <h2 className="text-red-500">Education</h2>
+          <h3 className="text-blue-500">Bachelor of Computer Science</h3>
+          <p>Institute of Technology of Cambodia (ITC), 2022 - Present</p>
+        </section>
+
+        <section id="skills" className="text-center py-10">
+          <h2 className="text-2xl font-semibold">Skills</h2>
+          <table className="w-full border-collapse border border-blue-500">
+            <thead>
+              <tr className="bg-cyan-200">
+                <th className="border border-red-400">Programming Languages</th>
+                <th className="border border-red-400">Frameworks</th>
+                <th className="border border-red-400">Tools</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-yellow-200">
+                <td className="border border-purple-400">
+                  JavaScript, TypeScript, Java
+                </td>
+                <td className="border border-purple-400">
+                  Next.js, Node.js, NestJS
+                </td>
+                <td className="border border-purple-400">Git, Docker, Nginx</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr className="bg-green-200">
+                <td className="border border-pink-500">PHP</td>
+                <td className="border border-pink-500">Laravel</td>
+                <td className="border border-pink-500">
+                  VS Code, Linux experience
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </section>
+
+        <section id="project" className="text-center py-10">
+          <h2 className="text-2xl font-semibold">Projects</h2>
+          <h3>Student Management System</h3>
+          <p>Using C++ to develop a system.</p>
+        </section>
+
+        <section id="languages" className="text-center py-10">
+          <h2 className="text-2xl font-semibold">Languages</h2>
+          <ul>
+            <li>Khmer: Mother tongue</li>
+            <li>English: Intermediate</li>
+          </ul>
+        </section>
+
+        <section id="references" className="text-center py-10">
+          <h2 className="text-2xl font-semibold">References</h2>
+          <p>Available upon request</p>
+        </section>
+
+        <section id="contact" className="text-center py-10">
+          <h2 className="text-2xl font-semibold">Contact Me</h2>
+          <p>Email: ponlork2535@gmail.com</p>
+          <p>Phone: +855 97 510 8710</p>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
